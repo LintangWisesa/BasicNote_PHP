d14c13354664d50b0da150d7388365cf03a63258
lintangwisesa <lintangwisesa@ymail.com>
2020-07-07 11:59:05 +0700

    user input

---
+++ 6_userinput_POST.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Fundamentals</title>
+</head>
+<body>
+    
+    <form action="6_userinput_POST.php" method="post">
+        Nama : <input type="text" name="nama">
+        <input type="submit" value="OK">
+    </form>
+    <br>
+    
+    <?php 
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $nama = $_POST["nama"];
+            echo "Nama Anda " . $nama;
+        }
+    ?>
+    
+</body>
+</html>
